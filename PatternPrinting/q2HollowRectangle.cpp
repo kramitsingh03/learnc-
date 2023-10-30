80dad330c9efa34553e323c5df4aaac6592d10c5
Amit <amitkumar03022004@gmail.com>
2023-10-31 00:19:26 +0530

    Hollow Rectangle

---
+++ PatternPrinting/q2HollowRectangle.cpp
@@ -0,0 +1,24 @@
+// ******
+// *    *  Rows = 5
+// *    *  column = 6
+// *    *
+// ******
+#include <iostream>
+using namespace std;
+int main(){
+    int row,col;
+    cout<<"Enter Row and Column :";
+    cin>>row>>col;
+    for(int i=1;i<=row;i++){
+        for(int j=1;j<=col;j++){
+            if(i == 1 || i == row || j == 1 || j == col ){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
